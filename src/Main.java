bec123af705fde73fd6b4301053943a25c98554b
Vladimr <mashkovvv11@gmail.com>
2022-08-04 23:38:51 +0500

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Домашка 1.5 Массивы
+        //Задача 1
+        int arr1[] = new int [3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2]=3;
+
+        double arr2[] = {1.57, 7.654, 9.986};
+
+        char xyz [] = new char [3];
+        xyz [0] = 'x';
+        xyz [1] = 'y';
+        xyz [2] = 'z';
+
+        //Задача 2
+        for (int i=0; i< arr1.length; i++){
+            if (i< (arr1.length-1)){
+                System.out.print (arr1[i]+", ");
+            } else {
+                System.out.println (arr1[i]+" ");}
+        }
+
+        for (int i=0; i< arr2.length; i++){
+            if (i< (arr2.length-1)){
+                System.out.print (arr2[i]+", ");
+            } else {
+                System.out.println (arr2[i]+" ");}
+        }
+
+        for (int i=0; i< xyz.length; i++){
+            if (i< (xyz.length-1)){
+                System.out.print (xyz[i]+", ");
+            } else {
+                System.out.println (xyz[i]+" ");}
+        }
+        System.out.println (" ");
+        // Задача 3
+        for (int i=arr1.length-1; i>=0; i--){
+            if (i>0){
+                System.out.print (arr1[i]+", ");
+            } else {
+                System.out.println (arr1[i]+" ");}
+        }
+
+        for (int i=arr2.length-1; i>=0; i--){
+            if (i>0){
+                System.out.print (arr2[i]+", ");
+            } else {
+                System.out.println (arr2[i]+" ");}
+        }
+
+        for (int i=xyz.length-1; i>=0; i--){
+            if (i>0){
+                System.out.print (xyz[i]+", ");
+            } else {
+                System.out.println (xyz[i]+" ");}
+        }
+        System.out.println (" ");
+
+        //Задача 4
+        for (int i=0; i< arr1.length; i++){
+            if (i< (arr1.length-1)){
+                if (arr1[i]%2==1){
+                    System.out.print ((arr1[i]+1)+", ");
+                }else{
+                    System.out.print (arr1[i]+", "); }
+            } else {
+                if (arr1[i]%2==1){
+                    System.out.print ((arr1[i]+1)+" ");
+                }else{
+                    System.out.println (arr1[i]+" ");}
+            }
+        }
     }
 }
